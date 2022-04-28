ffc568c8d8f71c0fddc4f5941dff59ca5f7637f6
salih abas <salihabas16@gmail.com>
2022-04-28 22:06:06 +0300

    AC1 completed

--- src/LoginTest_Salih.java
+++ src/LoginTest_Salih.java
@@ -1,5 +1,7 @@
 public class LoginTest_Salih {
     public static void main(String[] args) {
         System.out.println("Login class is created");
+
+        System.out.println("AC! testing is passed");
     }
 }
